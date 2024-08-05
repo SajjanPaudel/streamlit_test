b60b25f2c1143e036af440f2f3cd667e7eef4699
Sajjan <sajjanpaudel811@gmail.com>
2024-08-05 17:40:42 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title('🎈Testing streamlit for data science')
 
 with st.sidebar:
-  st.write('**SOMETHING**')
+  st.write('**PARAMETERS TO CHANGE**')
 with st.expander('DATA'):
   st.write('Hello world!')
