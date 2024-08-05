b2dfe8520c0fcb62f6ddcbb049423cd08952b63b
Sajjan <sajjanpaudel811@gmail.com>
2024-08-05 17:53:21 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈Testing streamlit for data science')
+restaurant_data = pd.read_csv('https://raw.githubusercontent.com/suyogdahal/KhajaTime/master/KhajaTime.csv')
 
 with st.sidebar:
   st.write('**PARAMETERS TO CHANGE**')
 with st.expander('DATA'):
   st.write('Hello world!')
+  restaurant_data
